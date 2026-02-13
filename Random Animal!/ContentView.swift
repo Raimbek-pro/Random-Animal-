a77f8148b4b38d90445d5ec9b9ead44243450c2c
Raiymbek <76900558+Raimbek-pro@users.noreply.github.com>
2026-02-14 00:50:50 +0500

    refactor and smooth animation

--- Random Animal!/ContentView.swift
+++ Random Animal!/ContentView.swift
@@ -24,115 +24,165 @@ struct ContentView: View {
     
     var itemRatings : [Int] = [3,5,2,4,5,0]
     
- 
+    var colorsGradient :[Color] =    [.teal,.red]
+    
+    func endOfGradient(_ index:Int) -> UnitPoint {
+        return UnitPoint(x: 0.5, y:3.5/CGFloat(index+1))
+    }
     
     @State var currentIndex : Int = 0
     @State var tapCount : Int = 0
     @State var isAnimating : Bool = false
     
-    var firstTime = 0
+    @State var isTransAnimating : Bool = false
     
+    var firstTime = 0
     
     var body: some View {
         
         ZStack{
             
-            LinearGradient(colors: [.teal,.red], startPoint: .top, endPoint: UnitPoint(x: 0.5, y:3.5/CGFloat(itemRatings[currentIndex]+1)))
-           
-                .ignoresSafeArea(.all)
+        LinearGradient(
+             colors:  colorsGradient,
+             startPoint: .top,
+             endPoint:  endOfGradient(itemRatings[currentIndex])
+        )
             
-            VStack {
-                
-                
-                Image(systemName: "\(itemIcons[currentIndex])")
-                    .font((.system(size: 80)))
-                    .scaleEffect(isAnimating ? 2.0 : 1.0)
-                
-                Text("\(itemNames[currentIndex])")
-                    .font(.title)
-                    .bold()
-                
-                Divider()
-                
-                Text("\(itemDescriptions[currentIndex])")
-                    .font(.body)
-                    .foregroundStyle(.secondary)
-                    .multilineTextAlignment(.center)
-                    .padding([.leading, .trailing],30)
-                
-                let stringRating =   itemRatings[currentIndex] != 0 ? String(repeating: "🐾", count: itemRatings[currentIndex]) : "Not rated yet"
-                
-                
-                
-                Text("Cuteness: \(stringRating)")
-                    .foregroundStyle(.secondary)
-                    .padding(10)
-                
-                Button(action: {
-                    var newIndex : Int
-                    repeat{
-                        newIndex  = Int.random(in: 0..<itemNames.count)
-                    } while newIndex == currentIndex
+        .ignoresSafeArea(.all)
+            
+      
+            
+                VStack {
+                    card
                     
                     
-                    withAnimation(.spring()) {
-                        isAnimating.toggle()
+                }
+                .id(currentIndex)
+                .transition(.slide)
+                
+                
+                .padding()
+            
+            
+        }
+    
+    }
+    
+
+}
+
+
+extension ContentView {
+    
+    @ViewBuilder
+   private var card : some View{
+        
+        
+        imageAnimal
+       
+        textAnimal
+       
+        Divider()
+       
+        descriptionAnimal
+        
+        rating
+       
+        button
+        
+        cardsexplored
+        
+    }
+    
+    
+}
+
+extension ContentView{
+    
+    private var imageAnimal : some View{
+        Image(systemName: "\(itemIcons[currentIndex])")
+            .font((.system(size: 80)))
+            .scaleEffect(isAnimating ? 2.0 : 1.0)
+    }
+    
+    private var textAnimal : some View{
+        Text("\(itemNames[currentIndex])")
+            .font(.title)
+            .bold()
+    }
+    
+    private var descriptionAnimal : some View{
+        Text("\(itemDescriptions[currentIndex])")
+            .font(.body)
+            .foregroundStyle(.secondary)
+            .multilineTextAlignment(.center)
+            .padding([.leading, .trailing],30)
+    }
+    
+    @ViewBuilder
+    private var rating : some View {
+        let stringRating =   itemRatings[currentIndex] != 0 ? String(repeating: "🐾", count: itemRatings[currentIndex]) : "Not rated yet"
+        
+        Text("Cuteness: \(stringRating)")
+            .foregroundStyle(.secondary)
+            .padding(10)
+
+    }
+    
+    
+    private var button : some View {
+        Button(action: {
+            var newIndex : Int
+            repeat{
+                newIndex  = Int.random(in: 0..<itemNames.count)
+            } while newIndex == currentIndex
+            
+           
+            withAnimation{
+                currentIndex = newIndex
+            }
+            completion :{
+                
+                withAnimation(.spring()) {
+                        
+                  
+                    isAnimating.toggle()
+                        
                         
                         
                     } completion: {
-                        
-                        withAnimation(.spring){
-                            isAnimating.toggle()
+                       
+                        withAnimation(.spring()){
+                                              isAnimating.toggle()
+                            
                         }
                     }
-                    
-                    
-                    
-                    
-                    currentIndex = newIndex
-                    
-                    
-                    tapCount += 1
-                    
-                }, label: {
-                    Text("Surpise me")
-                        .padding(12)
-                    //  .background(Color.blue)
-                    //   .foregroundStyle(Color.white)
-                    //   .clipShape(.capsule)
-                    
-                    
-                })
-                .buttonStyle(.borderedProminent)
-                
-                Text("Cards explored : \(tapCount)")
-                    .padding(10)
-                
-                
             }
+            
+            
             
             
           
             
             
+            tapCount += 1
             
+        }, label: {
+            Text("Surpise me")
+                .padding(12)
             
-            
-                .padding()
-            
-        }
-    
-        
-        
-        
-        
+        })
+        .buttonStyle(.borderedProminent)
     }
     
-//    private var back : some View {
-//      
-//    }
-
+    private var cardsexplored : some View{
+        
+        Text("Cards explored : \(tapCount)")
+            .padding(10)
+    }
 }
+
+
 
 
 #Preview {
